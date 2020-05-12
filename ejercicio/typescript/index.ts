8f8bd44e15d9415cb398e061a5aad05bf8adc1a7
JefferLindao <jefferlindao@gmail.com>
2020-05-12 16:18:57 -0500

    Tipo de datos Typescript

--- ejercicio/typescript/index.ts
+++ ejercicio/typescript/index.ts
@@ -1,9 +1,40 @@
-console.log('hello, typescript')
+/**
+ * tipo de dato booleano
+ */
+let muted: boolean = true 
+muted = false 
+/**
+ * tipo de dato number
+ */
+let age = 4
+let numerador: number = 43
+let denominador: number = age
+let resultado = numerador/denominador
+/**
+ * tipo de dato string
+ */
+let nombre: string = 'Jefferson'
+let saludo = `Me llamo ${nombre}`
+/**
+ * tipo arreglo
+ */
+let people: string[] = []
+people=['Jefferson','Fernado','Gonzalo']
 
-function add (a: number, b: number){
-    return a + b
+let peopleAndNumber: Array<string|number>=[]
+peopleAndNumber.push('Jose')
+peopleAndNumber.push(1523)
+// Enum
+enum Color{
+    Rojo = 'Rojo',
+    Verde = 'Verde',
+    Azul = 'Azul'
 }
-
-const sum = add(2, 4)
-
-console.log(`la suma es: ${sum}`)
+let colorFavorito: Color = Color.Verde
+console.log(`Mi color favorito es ${colorFavorito}`)
+// Any
+let comodin: any = 'Joker'
+// Object
+let someObject: object  = {
+    type:'Wildcard'
+}
